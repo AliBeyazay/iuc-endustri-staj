79f69f8f94f09445e9e6b347e8818306df80ca15
Ali Beyazay <beyazayali257@gmail.com>
2026-03-24 01:21:41 +0300

    Add Render deployment config, WhiteNoise static files, production-ready backend

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -3,8 +3,8 @@ import os
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-MIDDLEWARE = ['config.healthcheck.HealthcheckMiddleware', *MIDDLEWARE]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+MIDDLEWARE = ['config.healthcheck.HealthcheckMiddleware', 'django.middleware.security.SecurityMiddleware', 'whitenoise.middleware.WhiteNoiseMiddleware', *[m for m in MIDDLEWARE if m not in ('config.healthcheck.HealthcheckMiddleware', 'django.middleware.security.SecurityMiddleware')]]
 
 # Security
 SECURE_PROXY_SSL_HEADER    = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -16,8 +16,9 @@ SECURE_BROWSER_XSS_FILTER   = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS             = 'DENY'
 
-# Static files (use whitenoise or S3 in production)
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+# Static files via WhiteNoise
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     **REST_FRAMEWORK,
